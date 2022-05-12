62648d765090893bf91b5b7152894ae9bd43debe
우정무 <app.le@kakao.com>
2022-05-12 23:42:56 +0900

    initial commit

---
+++ src/test/java/com/chuibbo/plantodo/PlantodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chuibbo.plantodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantodoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
